46289d95a472e9da5c8ca288a1e6acff180b78a6
Яньшин Сергей Сергеевич <yanshin.s@medindex.ru>
2022-07-25 04:04:16 +0300

    fix(message): fix date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,9 @@
 export const formatChatMessageDate = (ISODate: string) => {
     const date = new Date(ISODate);
-    return `${date.getDay()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
+    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
 }
 
 export const formatLastMessageDate = (ISODate: string) => {
     const date = new Date(ISODate);
-    return `${date.getHours().toString().padEnd(2, '0')}:${date.getMinutes().toString().padEnd(2, '0')}`;
+    return `${date.getHours().toString().padEnd(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
 }
